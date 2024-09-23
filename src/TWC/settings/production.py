40e58653276275d6afa612918c3ea850d0bff485
Noy Pangan <noypangan5@gmail.com>
2024-09-23 08:28:34 +0800

    Production settings allowed host error fix

--- src/TWC/settings/production.py
+++ src/TWC/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['.twconline.store', '172.105.126.70', '139.144.121.152']
+ALLOWED_HOSTS = ['172.105.126.70', '139.144.121.152', '172.104.35.33', '.twconline.store', 'www.twconline.store']
 
 DASHBOARD_URL = 'https://dashboard.twconline.store'
 ADMIN_URL = 'https://admin.twconline.store'
