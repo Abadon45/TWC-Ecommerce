b84422ac38c6f70c24ed5f4ed9c0d95d63af5af4
Noy Pangan <noypangan5@gmail.com>
2024-10-01 08:49:01 +0800

    Test production.py by adding test.twconline.store

--- src/TWC/settings/production.py
+++ src/TWC/settings/production.py
@@ -2,7 +2,15 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['172.105.126.70', '139.144.121.152', '172.104.35.33', '.twconline.store', 'www.twconline.store']
+ALLOWED_HOSTS = [
+    '172.105.126.70',
+    '139.144.121.152',
+    '172.104.35.33',
+    '172.104.32.135',
+    '.twconline.store',
+    'www.twconline.store',
+    'test.twconline.store',
+]
 
 DASHBOARD_URL = 'https://dashboard.twconline.store'
 ADMIN_URL = 'https://admin.twconline.store'
