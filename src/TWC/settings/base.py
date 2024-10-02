91f94a324e328a52868476b8b09043d4a37dee44
Noy Pangan <noypangan5@gmail.com>
2024-10-02 10:51:10 +0800

    Fix server settings

--- src/TWC/settings/base.py
+++ src/TWC/settings/base.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages'
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_celery_beat',
     'django_celery_results',
