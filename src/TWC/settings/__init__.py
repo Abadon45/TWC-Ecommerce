3c19b4cdee1b7ab88b094a516c4aec99a9ef651d
Noy Pangan <noypangan5@gmail.com>
2024-10-02 13:30:58 +0800

    Removed local.py to push

--- src/TWC/settings/__init__.py
+++ src/TWC/settings/__init__.py
@@ -1,7 +1,11 @@
 from .base import *
 import os
 
-if os.getenv('DJANGO_ENV') == 'production':
-    from .production import *
-else:
+try:
+    # Try to load the local settings if it exists
     from .local import *
+    print("Loaded local settings")
+except ImportError:
+    # Fallback to production settings if local.py is not available
+    from .production import *
+    print("Loaded production settings")
