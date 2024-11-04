3a8373a31fcdbe611744be5bb75b608db6787d86
Noy Pangan <noypangan5@gmail.com>
2024-11-04 11:26:08 +0800

    Terms of service

--- src/TWC/urls/default.py
+++ src/TWC/urls/default.py
@@ -12,9 +12,17 @@ urlpatterns = [
         'return-policy/',
         TemplateView.as_view(
             template_name='return-policy.html',
-            extra_context={'title': 'Return Policy'}
+            extra_context={'title': 'TWC Store | Return Policy'}
         ),
         name="return_policy"
+    ),
+    path(
+        'terms-of-service/',
+        TemplateView.as_view(
+            template_name='terms-of-service.html',
+            extra_context={'title': 'TWC Store | Terms of Service'}
+        ),
+        name="terms_of_service"
     ),
     path('test-email/', EmailFormView.as_view(), name="test_email"),
     path('login/', include('TWC.urls.login', namespace='login')),
