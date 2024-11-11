1e045a4674f75984534f58e6543286b423e86f14
Noy Pangan <noypangan5@gmail.com>
2024-11-11 15:22:13 +0800

    Main store address fix

--- src/cart/views.py
+++ src/cart/views.py
@@ -301,7 +301,8 @@ class CheckoutView(View):
         }
 
         # Save the address to session
-        del self.request.session['shipping_address']
+        if "shipping_address" in self.request.session:
+            del self.request.session['shipping_address']
         self.request.session['shipping_address'] = shipping_address
         print(f"Address: {shipping_address}")
 
