aa84df5c87ea88fe57aaefee3552c2b04348bdd8
Noy Pangan <noypangan5@gmail.com>
2025-01-16 11:02:26 +0800

    Shop api changed

--- src/TWC/settings/base.py
+++ src/TWC/settings/base.py
@@ -22,7 +22,7 @@ XENDIT_API_KEY = os.environ.get("XENDIT_API_KEY", '_gyFfI1cqWWOTpXRWcfg1RMPC3UkC
 HOST_DOMAIN = os.environ.get("HOST_DOMAIN", "twcako")
 
 # TWCAKO API
-SHOP_PRODUCTS_API = f'https://dashboard.twcako.com/shop/api/get-product/?'
+SHOP_PRODUCTS_API = f'https://dashboard.{HOST_DOMAIN}.com/shop/api/get-product/?'
 PRODUCT_URL_API = f'https://dashboard.twcako.com/shop/api/get-product/?slug='
 REFRESH_TOKEN_API = f'https://dashboard.{HOST_DOMAIN}.com/order/api/get-access-token/'
 ORDER_URL_API = f'https://dashboard.{HOST_DOMAIN}.com/order/api/create-order/'
