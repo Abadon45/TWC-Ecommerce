49e737f63cf3a29fe8d00870367fadadaa5d9faa
Noy Pangan <noypangan5@gmail.com>
2025-01-25 13:06:58 +0800

    CORS setting update

--- src/TWC/settings/production.py
+++ src/TWC/settings/production.py
@@ -55,3 +55,13 @@ ORDER_API = 'https://dashboard.twcako.com/order/api/create-order/'
 DASHBOARD_URL = 'https://dashboard.twconline.store'
 ADMIN_URL = 'https://admin.twconline.store'
 MAIN_SITE_URL = 'https://www.twconline.store'
+
+
+CORS_REPLACE_HTTPS_REFERER      = True
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
