e47b710d9ba7f5dae06f3c55008497e4259f75c7
Noy Pangan <noypangan5@gmail.com>
2025-03-12 06:44:48 +0800

    Resolve URL for login 7

--- src/TWC/urls/default.py
+++ src/TWC/urls/default.py
@@ -22,7 +22,7 @@ urlpatterns = [
         name="terms_of_service"
     ),
     path("user/", include("TWC.urls.dashboard", namespace="dashboard")),
-    # path("login/", APILoginView.as_view(), name="login"),
+    path("login/", APILoginView.as_view(), name="login"),
     path('shop/', include('TWC.urls.shop', namespace='shop')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('accounts/', include('allauth.urls')),
