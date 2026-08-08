80046fe8af0e1d8b5e5813a5dfb8ae60d0adab5a
Abadon45 <noypangan5@gmail.com>
2026-08-09 00:08:53 +0800

    fix(security): move SendGrid credentials to environment

--- src/TWC/settings/base.py
+++ src/TWC/settings/base.py
@@ -231,15 +231,13 @@ LOGOUT_REDIRECT_URL = "/"
 
 # SENDGRID EMAIL
 
-SENDGRID_API_KEY = ''
-username = 'ZXZnZXJvbmlsbGE='
-password = 'dmVuZGljczIwMTU='
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
 EMAIL_MAIN = 'TWCAKO <support@twcako.com>'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY", "")
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 
